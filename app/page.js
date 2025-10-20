233038bfffb3d36e76fc2e3d7c24e9269b78cb5d
hamsoool <hanslacuesta@gmail.com>
2025-10-20 11:36:01 +0800

    sanaerl

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      
+      {/* sd */}
       <section className={styles.hero}>
         <Image 
           src="/images/bg.jpg" 
